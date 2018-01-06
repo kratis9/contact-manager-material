c56e5011c827929c38070f7bc537ecaee0247274
krati <kratis9@gmail.com>
2018-01-06 23:39:34 +1100

    add flexbox and button demo component

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -5,10 +5,13 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 const routes: Routes = [
   { path: 'buttons', component: ButtonsComponent },
+  { path: 'flexbox', component: FlexboxComponent },
   { path: '**', redirectTo: 'demo' }
 ];
 
@@ -20,6 +23,6 @@ const routes: Routes = [
     MaterialModule,
     FormsModule
   ],
-  declarations: [ButtonsComponent]
+  declarations: [ButtonsComponent, FlexboxComponent]
 })
 export class DemoModule { }
